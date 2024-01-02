5a535fe8e6065201e3ac48ca3986809fe6adb4d7
AbduHany <abdu.hany@gmail.com>
2024-01-02 23:04:07 +0200

    Add 13-insert_number.c

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,65 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * place_node - places the node at the right place in the list.
+ */
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list.
+ * @head: pointer to the head pointer of the linked list.
+ * @number: the number to be insterted to the list.
+ *
+ * Return: the address of the new node or NULL if failed.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *tmp1, *tmp2, *new;
+
+	/** check if head or *head is NULL **/
+	if (head == NULL)
+		return (NULL);
+	/** create a new node with number **/
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	/** assigning the values to the node **/
+	new->n = number;
+	new->next = NULL;
+	/** assigning 2 tmp pointers **/
+	tmp1 = *head;
+	tmp2 = (*head)->next;
+	/** if list has no nodes **/
+	if (tmp1 == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	/** if list has one node **/
+	else if (tmp2 == NULL)
+	{
+		if ((*head)->n > number)
+		{
+			*head = new;
+			new->next = tmp1;
+			return (new);
+		}
+		else
+		{
+			(*head)->next = new;
+			return (new);
+		}
+	}
+	while (tmp2 != NULL)
+	{
+		if (tmp1->n <= number && tmp2->n >= number)
+		{
+			tmp1->next = new;
+			new->next = tmp2;
+			return (new);
+		}
+		tmp1 = tmp1->next;
+		tmp2 = tmp2->next;
+	}
+	return (new);
+}
