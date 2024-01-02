7eed165f6ec1d8e5d47c08fa865b4f437a67a1cd
AbduHany <abdu.hany@gmail.com>
2024-01-02 17:30:15 +0200

    Modify 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if (i < 10):
-        print("{:02d}, ".format(i), end="")
-    elif ((i % 10) == 0):
-        i += (i + 1)
-        continue
-    elif (i == 89):
-        print("{:02d}".format(i))
-        break
-    else:
-        print("{:02d}, ".format(i), end="")
+for i in range(0, 10):
+    for j in range(0, 10):
+        if (i == j):
+            continue
+        elif (j < i):
+            continue
+        else:
+            if ((i == 8) and (j == 9)):
+                print("{}{}".format(i, j))
+                break
+            print("{}{}, ".format(i, j), end="")
