12a94835bf56adbb6bbcbca13ff2aac3bb82674c
AbduHany <abdu.hany@gmail.com>
2024-01-02 23:49:47 +0200

    Add 100-print_tebhahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+num = 122
+for i in range (0, 26):
+    if (i == 0):
+        pass
+    elif ((i % 2) == 0):
+        num = num - 1 + 32
+    else:
+        num = num - 1 - 32
+    print("{}".format(chr(num)), end="")
