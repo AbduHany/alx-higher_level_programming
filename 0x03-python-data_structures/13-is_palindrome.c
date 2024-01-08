5519b3d34eeeede9f1db8dba0d63ddd321b27c92
AbduHany <abdu.hany@gmail.com>
2024-01-08 02:43:09 +0200

    Modify 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,6 +3,27 @@
 #include <stdio.h>
 
 /**
+ * test - tests if string characters at end and start are alike.
+ * @start: string pointer to be tested
+ * @end: string length.
+ * @max: number of tests.
+ * Return: 1 is all characters match.
+ * 0 if otherwise.
+ */
+int test(int *start, int *end, int max)
+{
+	if (max == 0)
+	{
+		return (1);
+	}
+	else if (*(start) != *(end))
+	{
+		return (0);
+	}
+	return (test(start + 1, end - 1, max - 1));
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome.
  * @head: pointer to the head pointer of the signly linked list.
  *
@@ -10,7 +31,7 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int *list = NULL, count = 1, items, i = 0, paliflag = 1;
+	int *list = NULL, count = 1, items, paliflag = 0;
 	listint_t *temp;
 
 	if (head == NULL || *head == NULL || (*head)->next == NULL)
@@ -30,13 +51,7 @@ int is_palindrome(listint_t **head)
 		temp = temp->next;
 	}
 	items = count - 1;
-	for (i = 0; i < (items / 2); i++)
-	{
-		if (list[items - 1 - i] == list[i])
-			continue;
-		else
-			paliflag = 0;
-	}
+	paliflag = test(&list[0], &list[items - 1], (items / 2));
 	free(list);
 	return (paliflag);
 }
