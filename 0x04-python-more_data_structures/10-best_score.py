b50e591bafecc2bdd708d45db06628599b0d0920
AbduHany <abdu.hany@gmail.com>
2024-01-10 22:07:09 +0200

    Add 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if (a_dictionary is None):
+        return None
+    sortedvals = list(reversed(sorted(a_dictionary.values())))
+    bestscore = sortedvals[0]
+    for i, j in a_dictionary.items():
+        if j == bestscore:
+            return (i)
