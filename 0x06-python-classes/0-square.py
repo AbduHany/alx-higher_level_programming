b57055973e8fa0c90c079b7d7ccbcebce92c4ec2
AbduHany <abdu.hany@gmail.com>
2024-01-23 21:08:21 +0200

    Modify 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" This module defines the empty class named Square
+"""
+This module defines the empty class named Square
 that defines a square.
 """
 
+
 class Square:
-    """ empty class named Square """
-    def __init__(self):
-        """ This method initializes an Instance object of class Square
-        """
-        pass
+    """
+    This is an empty class named Square that defines a square.
+    """
+    pass
