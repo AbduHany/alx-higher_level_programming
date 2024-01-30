b011ad55eec97ecef7def128ab4958ebb5d8dd10
AbduHany <abdu.hany@gmail.com>
2024-01-30 23:55:57 +0200

    Change permission for 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""This module defines a class that's locked
+to only contain the attribute ``first_name``
+"""
 class LockedClass(object):
+    """This class is a locked class that could
+    only hold an attribute named ``first_name``
+    """
     __slots__ = ['first_name']
-
-    def __init__(self):
-        pass
