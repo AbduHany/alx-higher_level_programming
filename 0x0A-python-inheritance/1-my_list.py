eddf2b91642d8562c8dcba63b4d192ac9cbf64c4
AbduHany <abdu.hany@gmail.com>
2024-02-05 21:56:23 +0200

    Modify tests/1-my_list.txt

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,8 @@ from the class list.
 
 class MyList(list):
     """This subclass is based off the class ``list``
+    and it inherits all its fields and methods and adds
+    a new method ``print_sorted`` that prints the list sorted.
     """
     def print_sorted(self):
         """This method prints a copy of the list object, but sorted.
