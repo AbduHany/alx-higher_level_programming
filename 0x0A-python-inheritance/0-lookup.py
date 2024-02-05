1e24d0918721a750e2b8c5ad5f3198eded059d01
AbduHany <abdu.hany@gmail.com>
2024-02-05 21:16:43 +0200

    Modify 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+""" This module defines the function ``lookup``
+"""
+
+
 def lookup(obj):
+    """This function returns a list of all attributes
+    attached to an object.
+    """
     return dir(obj)
