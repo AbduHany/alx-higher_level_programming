748c8da65bb5d45eced6523c3dcba59a7f5313e1
AbduHany <abdu.hany@gmail.com>
2024-02-05 23:30:20 +0200

    Modify 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,12 @@
 
 
 def inherits_from(obj, a_class):
+    """This function returns True if ``obj`` is inherited from
+    ``a_class``
+    Args:
+        obj (object): the objects that's being tested.
+        a_class (class object): the class that we're testing against.
+    """
     if issubclass(obj.__class__, a_class.__bases__):
         return True
     else:
