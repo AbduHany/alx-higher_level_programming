407a58aa0707c9a71b4db18b6d2b29235e34713a
AbduHany <abdu.hany@gmail.com>
2024-02-05 22:05:57 +0200

    Modify 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,12 +9,8 @@ class MyList(list):
     and it inherits all its fields and methods and adds
     a new method ``print_sorted`` that prints the list sorted.
     """
-    def __init__(self):
-        """This method initializes a MyList object based on its
-        parent's __init__ method.
-        """
-        super().__init__()
 
+    
     def print_sorted(self):
         """This method prints a copy of the list object, but sorted.
         """
