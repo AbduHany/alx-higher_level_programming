8faa1603def03a1069ae88187a641414dd706a6a
AbduHany <abdu.hany@gmail.com>
2024-02-07 00:53:37 +0200

    Add 100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines the function ``append_after``.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """This function inserts a line of text to a file, after each line
+    containing a specific string.
+    Args:
+        filename (str): name of file to be appended to.
+        search_string (str): string to be searched.
+        new_string (str): string to be appended after searched string.
+    """
+    with open(filename, "r", encoding='utf-8') as f:
+        textlist = f.readlines()
+    with open(filename, "a", encoding='utf-8') as f:
+        f.truncate(0)
+        for i in range(len(textlist)):
+            if "Python" in textlist[i]:
+                textlist.insert(i + 1, new_string)
+        f.writelines(textlist)
