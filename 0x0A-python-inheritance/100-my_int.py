bc1b556a671d3210c02ef1fa79693a24f2f159a2
AbduHany <abdu.hany@gmail.com>
2024-02-06 02:26:02 +0200

    Modify 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,9 +4,20 @@
 
 
 class MyInt(int):
-
-
+    """This class defines a rebel class ``MyInt``
+    that acts as a rebel to its parent ``int`` and
+    has 2 methods.
+    """
     def __eq__(self, value):
-            return not int.__eq__(self, value)
+        """This method returns false if MyInt and other value are equal.
+        Args:
+            value (int): value that MyInt object will be compared to.
+        """
+        return not int.__eq__(self, value)
+
     def __ne__(self, value):
-            return not int.__ne__(self, value)
+        """This method returns True if MyInt and other value are equal.
+        Args:
+            value (int): value that MyInt object will be compared to.
+        """
+        return not int.__ne__(self, value)
