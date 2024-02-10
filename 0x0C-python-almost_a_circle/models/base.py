a1ebaedb66208a1ffe7645492904501b64824778
AbduHany <abdu.hany@gmail.com>
2024-02-10 17:55:17 +0200

    Add the to_json_string staticmethod to base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class named ``Base``.
 """
+import json
 
 
 class Base:
@@ -20,3 +21,9 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if (list_dictionaries is None) or (len(list_dictionaries) == 0):
+            return ("[]")
+        return (json.dumps(list_dictionaries))
