80dabb78e837fb764fd2cc1d194f985e5918a5b0
AbduHany <abdu.hany@gmail.com>
2024-02-12 15:19:03 +0200

    Add unittests to test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,6 +4,7 @@
 from models.rectangle import Rectangle
 from models.square import Square
 from models.base import Base
+import json
 import unittest
 
 
@@ -81,5 +82,80 @@ class base_init_test(unittest.TestCase):
 
 class base_tojsonstring_test(unittest.TestCase):
     """This method tests the ``to_json_string`` method of the base class.
+    Testing is done and compared to the standard json.dumps method.
     """
-    
+    def test_rec_dictlist(self):
+        rec1 = Rectangle(1, 4, 5, 0, 1)
+        rec2 = Rectangle(5, 5, 6, 1, 0)
+        dictlist = [rec1.to_dictionary(), rec2.to_dictionary()]
+        self.assertEqual(Base.to_json_string(dictlist), json.dumps(dictlist))
+
+    def test_square_dictlist(self):
+        squ1 = Square(4, 5, 0, 1)
+        squ2 = Square(5, 5, 6)
+        dictlist = [squ1.to_dictionary(), squ2.to_dictionary()]
+        self.assertEqual(Base.to_json_string(dictlist), json.dumps(dictlist))
+
+    def test_mixed_dictlist(self):
+        squ = Square(5, 6, 0, 1)
+        rec = Rectangle(2, 4, 5, 1, 0)
+        dictlist = [squ.to_dictionary(), rec.to_dictionary()]
+        self.assertEqual(Base.to_json_string(dictlist), json.dumps(dictlist))
+
+    def test_emptylist(self):
+        dictlist = []
+        self.assertEqual(Base.to_json_string(dictlist), "[]")
+
+    def test_Nonelist(self):
+        dictlist = None
+        self.assertEqual(Base.to_json_string(dictlist), "[]")
+
+    def test_no_dictlist(self):
+        dictlist = [1, "Hello", 4.5]
+        self.assertEqual(Base.to_json_string(dictlist), '[1, "Hello", 4.5]')
+
+    def test_singledictarg(self):
+        dictlist = dict(id=3, width=4, height=5, x=2, y=4)
+        self.assertEqual(Base.to_json_string(dictlist), json.dumps(dictlist))
+
+    def test_stringarg(self):
+        dictlist = "Abdu"
+        self.assertEqual(Base.to_json_string(dictlist), '"Abdu"')
+
+    def test_intarg(self):
+        dictlist = 500
+        with self.assertRaises(TypeError):
+            Base.to_json_string(dictlist)
+
+    def test_floatarg(self):
+        dictlist = 2.3
+        with self.assertRaises(TypeError):
+            Base.to_json_string(dictlist)
+
+    def test_tuplearg(self):
+        dictlist = (5, 6, 7)
+        self.assertEqual(Base.to_json_string(dictlist), '[5, 6, 7]')
+
+    def test_setarg(self):
+        dictlist = {1, 3, 3, 5}
+        with self.assertRaises(TypeError):
+            Base.to_json_string(dictlist)
+
+    def test_listoflists(self):
+        dictlist = [[dict(id=1, size=4, x=2, y=1)],
+                    [dict(id=4, size=6, x=0, y=3)]]
+        self.assertEqual(Base.to_json_string(dictlist), json.dumps(dictlist))
+
+    def test_emptyargs(self):
+        self.assertEqual(Base.to_json_string(), 1)
+
+    def test_morethanonearg(self):
+        dictlist = [dict(id=1, size=5, x=2, y=1)]
+        dictlist2 = [dict(id=3, size=10, x=3, y=5)]
+        self.assertEqual(Base.to_json_string(dictlist, dictlist2), 1)
+
+class base_savetofile_test(unittest.TestCase):
+    """This class tests the ``load_from_file``
+    """
+    def test_em(self):
+        pass
