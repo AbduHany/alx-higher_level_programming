6ec2d998110b8a2f9015d2bad3876342b1365d9f
AbduHany <abdu.hany@gmail.com>
2024-04-08 23:57:27 +0200

    Modify no-argument/1-argument behavior 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const len = process.argv.length;
 if (len === 2 || len === 3) {
-  console.log(1);
+  console.log(0);
 } else {
   const sortedArr = process.argv.sort();
   console.log(sortedArr[len - 2]);
