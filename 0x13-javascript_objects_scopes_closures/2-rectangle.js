9f70ecca87f62b1d692ca283eae465bbf182befc
AbduHany <abdu.hany@gmail.com>
2024-04-09 21:43:39 +0200

    Modify 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (!(h <= 0 || w <= 0 || typeof w !== 'number' ||
-    typeof h !== 'number')) {
+    if (w > 0 && h > 0) {
       this.height = h;
       this.width = w;
     }
