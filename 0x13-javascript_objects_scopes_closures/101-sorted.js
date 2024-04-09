8653d1f4ccc7da5d3f2ae21f7b5c223cd85486ac
AbduHany <abdu.hany@gmail.com>
2024-04-10 00:50:36 +0200

    Add 101-sorted.js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+let uniqueList = new Set(Object.values(dict));
+uniqueList = Array.from(uniqueList);
+console.log(uniqueList);
+for (let )
