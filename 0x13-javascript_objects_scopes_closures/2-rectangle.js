46ceb180b59cc7cccc47e0337fd945e0589594ad
AbduHany <abdu.hany@gmail.com>
2024-04-09 21:36:59 +0200

    Modify to return empty when given not number 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (!(h <= 0 || w <= 0 || h === undefined || w === undefined)) {
+    if (!(h <= 0 || w <= 0 || typeof w !== 'number' ||
+    typeof h !== 'number')) {
       this.height = h;
       this.width = w;
     }
