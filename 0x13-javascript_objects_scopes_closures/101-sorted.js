d863f0ab4d1914da7fbbcb2f053ce8e0911b9596
AbduHany <abdu.hany@gmail.com>
2024-04-10 02:18:15 +0200

    Add 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,5 +2,14 @@
 const dict = require('./101-data.js').dict;
 let uniqueList = new Set(Object.values(dict));
 uniqueList = Array.from(uniqueList);
-console.log(uniqueList);
-for (let )
+const newDict = {};
+for (const element of uniqueList) {
+  const valueArray = [];
+  for (const key of Object.keys(dict)) {
+    if (dict[key] === element) {
+      valueArray.push(key);
+    }
+    newDict[element] = valueArray;
+  }
+}
+console.log(newDict);
