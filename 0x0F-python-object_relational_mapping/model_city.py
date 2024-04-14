73d3c8f94344955a1be38c66a78769526b731cd7
AbduHany <abdu.hany@gmail.com>
2024-04-14 21:37:50 +0200

    Add 14-model_city_fetch_by_state.py & model_city.py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines the class City and the declarative instance Base.
+"""
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """This class defines the mapper class for the cities tables
+    in the hbtn_0e_14_usa DB.
+    """
+    __tablename__ = "cities"
+    id = Column('id', Integer, primary_key=True)
+    name = Column('name', String(128), nullable=False)
+    state_id = Column('state_id', Integer, ForeignKey('states.id'))
+
+    states = relationship('State', back_populates='cities')
+
+
+State.cities = relationship('City', back_populates='states')
