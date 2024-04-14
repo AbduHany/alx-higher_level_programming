10f6d56258d4962bb21f066e7328570538a78333
AbduHany <abdu.hany@gmail.com>
2024-04-14 18:09:51 +0200

    Modify model_state.py: fix pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,7 +4,10 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 
+
 Base = declarative_base()
+
+
 class State(Base):
     """This class defines the mapper class for the states tables
     in the hbtn_0e_6_usa DB.
