47811a6593851baf10cbdb1fdd6bd29a542b4725
AbduHany <abdu.hany@gmail.com>
2024-04-14 18:06:57 +0200

    Modify model_state.py: Add documentation State class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,6 +5,9 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 class State(Base):
+    """This class defines the mapper class for the states tables
+    in the hbtn_0e_6_usa DB.
+    """
     __tablename__ = "states"
     id = Column('id', Integer, primary_key=True)
     name = Column('name', String(128), nullable=False)
