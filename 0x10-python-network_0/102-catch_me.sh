99ebed0bda08d3c48698bea72108ca096ec61929
AbduHany <abdu.hany@gmail.com>
2024-04-26 14:59:49 -0700

    Modify 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl 0.0.0.0:5000/catch_me -s -X PUT -L -d "user_id=98"
+# causes the server to respond with a message containing You got me!
+curl 0.0.0.0:5000/catch_me -s -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool"
