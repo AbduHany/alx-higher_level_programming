f23dcf9d134d2675d4c3cb47924ae0d5b2645325
AbduHany <abdu.hany@gmail.com>
2024-04-26 15:01:00 -0700

    Modify 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # causes the server to respond with a message containing You got me!
-curl 0.0.0.0:5000/catch_me -s -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool"
+curl 0.0.0.0:5000/catch_me -s -X PUT -L -d "user_id=98" -H "Origin: School"
