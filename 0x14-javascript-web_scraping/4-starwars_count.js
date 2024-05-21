019b82b42df30984372e6ce7948d5af800d4366f
AbduHany <abdu.hany@gmail.com>
2024-05-21 17:43:18 +0300

    Modify 4-starwards_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,10 +7,10 @@ request(url, (err, response, body) => {
   if (err) {
     console.error(err);
   }
-  const charUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+  let charUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
   let count = 0;
-  const dict = JSON.parse(body);
-  for (const idx in dict.results) {
+  let dict = JSON.parse(body);
+  for (let idx in dict.results) {
     if (dict.results[idx].characters.includes(charUrl)) {
       count++;
     }
